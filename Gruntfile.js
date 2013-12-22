5d77f7601801e391d5658cc50ca34886bd78e55e
mytory <mytory@gmail.com>
2013-12-22 23:42:23 +0900

    add watch to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch']);
 
 };
