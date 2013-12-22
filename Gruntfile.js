736f57c6b53612d09f960403f93cd21f54b51cdc
mytory <mytory@gmail.com>
2013-12-22 21:53:53 +0900

    initial.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        concat: {
+            dist: {
+                src: [
+                    'js/src/jquery.min.js',
+                    'js/src/bootstrap.min.js',
+                    'js/src/underscore-min.js',
+                    'js/src/mytory-moneybook.js'
+                ],
+                dest: 'js/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/production.js',
+                dest: 'js/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/src/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js', 'js/src/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
+};
