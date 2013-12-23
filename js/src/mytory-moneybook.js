09020f53addf462b73cc53bfb51e924c7c3aa120
mytory <mytory@gmail.com>
2013-12-23 18:41:56 +0900

    Added account field.

--- js/src/mytory-moneybook.js
+++ js/src/mytory-moneybook.js
@@ -1,7 +1,7 @@
 var MMB = {
     initialize: function(){
         this.fill_today();
-        this.provide_category_data_source();
+        this.provide_data_source();
     },
     fill_today: function(){
         var date = new Date(),
@@ -9,8 +9,9 @@ var MMB = {
             date_string = date.getFullYear() + '-' + month + '-' + date.getDate();
         $('.js-today').val(date_string);
     },
-    provide_category_data_source: function(){
+    provide_data_source: function(){
         $('.js-category').data('source', ['Food:Dining', 'Food:Morning']);
+        $('.js-account').data('source', ['My Wallet', 'Hana Bank']);
     }
 };
 
