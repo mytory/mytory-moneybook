cd5d07e510f3b52b60f7971479e79bdaec12f61a
mytory <mytory@gmail.com>
2013-12-23 18:03:13 +0900

    Added menus. Changed write to regitster.

--- js/src/mytory-moneybook.js
+++ js/src/mytory-moneybook.js
@@ -1,13 +1,12 @@
 var MMB = {
     initialize: function(){
         this.fill_today();
-        console.log('initialized.');
     },
     fill_today: function(){
         var date = new Date(),
             month = date.getMonth() + 1,
             date_string = date.getFullYear() + '-' + month + '-' + date.getDate();
-        $('.js-date-today').val(date_string);
+        $('.js-today').val(date_string);
     }
 };
 
