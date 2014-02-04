043a84f28a6de39b8d262ec301e284aaa04ed7d9
mytory <mytory@gmail.com>
2014-02-04 11:16:40 +0900

    Fix balance bug.

--- js/src/mmb-backbone.js
+++ js/src/mmb-backbone.js
@@ -890,16 +890,19 @@ var MMB_Backbone = {
             // 시나리오
             // 1. 그냥 수입 : 자산과 잔액에 더하면 된다.
             // 2. 그냥 지출 : 자산과 잔액에 빼면 된다.
-            // 3. 은행에서 내 계좌로 이체(대출) : 은행은 마이너스 통장이 된다. 자산은 특별히 신경쓸 것 없다. 다만 은행계좌의 돈을 잔액에 포함하면 안 된다.
-            // 4. 내 계좌에서 은행으로 이체(대출 상환) : 위와 마찬가지.
+            // 3. 은행에서 내 계좌로 이체(대출) : 은행은 마이너스 통장이 된다. 자산은 특별히 신경쓸 것 없다.
+            //      다만 은행계좌의 돈을 잔액에 포함하면 안 된다. 수입이 된다.
+            // 4. 내 계좌에서 은행으로 이체(대출 상환) : 자산은 마찬가지. 지출이 된다.
             // 5. 내 계좌에서 친구 빌려줌 계좌로 이체 : 친구 빌려줌 계좌는 잔액에 포함하면 안 된다. 자산은 그대로.
-            // 6. 적금 : 자산에는 포함. 잔액엔 포함 안 한다.
+            //      지출이 되면 안 된다.
+            // 6. 적금 : 자산에는 포함. 잔액엔 포함 안 한다. 지출이 되면 안 된다.
 
             var balance = 0,
                 account,
                 to_account;
 
             _.forEach(list, function(item){
+
                 account = MMB.datastore.account_list.get(item.get('account_id'));
                 if(item.get('to_account_id')){
                     to_account = MMB.datastore.account_list.get(item.get('to_account_id'));
@@ -907,24 +910,25 @@ var MMB_Backbone = {
 
                 switch(item.get('behavior_type')){
                     case 'withdrawal':
-                        if(account.in_balance === 'no'){
+                        if(account.get('in_balance') === 'no'){
                             return true;
                         }
                         balance -= item.get('amount');
+
                         break;
 
                     case 'deposit':
-                        if(account.in_balance === 'no'){
+                        if(account.get('in_balance') === 'no'){
                             return true;
                         }
                         balance += item.get('amount');
                         break;
 
                     case 'transfer':
-                        if(account.in_balance === 'yes'){
+                        if(account.get('in_balance') === 'yes'){
                             balance -= item.get('amount');
                         }
-                        if(to_account.in_balance === 'yes'){
+                        if(to_account.get('in_balance') === 'yes'){
                             balance += item.get('amount');
                         }
 
